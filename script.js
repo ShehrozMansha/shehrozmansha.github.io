b14b5fab5fac36f5e56ea0a5f1d18d17ef4696b1
ShehrozMansha <gemstargs5067@gmail.com>
2026-06-21 20:03:08 +0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load',()=>{
-setTimeout(()=>document.getElementById('loader').style.display='none',1000);
-});
-document.getElementById('themeToggle').onclick=()=>{
-document.body.classList.toggle('light');
-};
